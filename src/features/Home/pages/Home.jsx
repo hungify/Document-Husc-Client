e8364c1f5b0183ba93379732422ae75d5619443d
hung purdie <nmhung.dev@gmail.com>
2022-04-08 09:30:23 +0700

    feat(home): change tree select mapping dummy data

--- src/features/Home/pages/Home.jsx
+++ src/features/Home/pages/Home.jsx
@@ -1,27 +1,17 @@
 import { Layout } from "antd";
 import SearchFilter from "components/SearchFilter";
-import GuestLayout from "layout/GuestLayout";
-
 import DocumentList from "features/Home/pages/DocumentList";
 import React from "react";
-import styled from "styled-components";
-const { Header, Footer, Content } = Layout;
-
-const HeaderWrapper = styled(Header)`
-  background-color: #fff;
-  height: 100%;
-`;
 
 export default function Home() {
   return (
-    <>
+    <Layout>
       <SearchFilter />
       <Layout>
-        <Content>
+        <Layout.Content>
           <DocumentList />
-        </Content>
+        </Layout.Content>
       </Layout>
-      <Footer>Footer</Footer>
-    </>
+    </Layout>
   );
 }
