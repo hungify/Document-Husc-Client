b547995f914d07906e957ae34a13c796e3413bbb
hung purdie <nmhung.dev@gmail.com>
2022-04-13 16:20:24 +0700

    fix(inbox-detail): ui list receiver and sent document

--- src/features/SentDocument/SentDocument.jsx
+++ src/features/SentDocument/SentDocument.jsx
@@ -1,75 +1,137 @@
-import { Avatar, Card, Col, List, Row, Typography } from "antd";
+import { BulbFilled } from "@ant-design/icons";
+import { Avatar, Badge, Card, Col, Divider, List, Radio, Row, Tag, Typography } from "antd";
+import _ from "lodash";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-const ColAnt = styled(Col)`
-  :not(:first-child) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+const CardAnt = styled(Card)`
+  transition: all 0.3s ease;
+  padding-left: 0;
+  padding-right: 0;
+  &:hover {
+    cursor: pointer;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    background-color: rgba(65, 132, 228, 0.15);
   }
 `;
+const WrapEllipsis = styled.div`
+  width: 100%;
+`;
 
-const listForwardData = [];
-for (let i = 0; i < 23; i++) {
-  listForwardData.push({
-    id: i + 1,
-    title: `Văn bản đã chuyển tiếp thứ ${i}`,
-    myMessage: "This is my message",
-    message: "Toàn thể khoa hãy triển khoai kế hoạch như trong văn bản.",
-    publisher: {
+const Ellipsis = styled.span`
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+`;
+
+const listToData = [];
+for (let i = 0; i < 40; i++) {
+  listToData.push(
+    {
       name: "Nguyễn Mạnh Tuấn",
-      avatar: "https://joeschmoe.io/api/v1/random",
-      email: `email.publisher.${i}@husc.edu.vn`,
-    },
-    to: {
-      name: "Hồng Nhung",
       avatar: "https://joeschmoe.io/api/v1/random",
       email: `email.receiver.${i}@husc.edu.vn`,
     },
+    {
+      name: "Nguyễn Mạnh Hải",
+      avatar: "https://joeschmoe.io/api/v1/random",
+      email: `email.receiver.${i}@husc.edu.vn`,
+    },
+    {
+      name: "Nguyễn Tuấn Tài",
+      avatar: "https://joeschmoe.io/api/v1/random",
+      email: `email.receiver.${i}@husc.edu.vn`,
+    },
+    {
+      name: "Nguyễn Tuấn Anh",
+      avatar: "https://joeschmoe.io/api/v1/random",
+      email: `email.receiver.${i}@husc.edu.vn`,
+    }
+  );
+}
+
+const listInboxData = [];
+for (let i = 0; i < 23; i++) {
+  listInboxData.push({
+    id: i + 1,
+    title: `
+21/NQ-HĐĐH : Nghị quyết về việc công nhận Hiệu trưởng Trường Đại học Y - Dược, Đại học Huế nhiệm kỳ 2020 - 2025 - ${i}`,
+    summary:
+      "Căn cứ Nghị định số 30/CP ngày 04 tháng 4 năm 1994 của Chính phủ về việc thành lập Đại học Huế; Căn cứ Thông tư số 10/2020/TT-BGDĐT ngày 14 tháng 5 năm 2020 của Bộ trưởng Bộ Giáo dục và Đào tạo ban hành Quy chế tổ chức và hoạt động của đại học vùng và các cơ sở giáo dục đại học thành viên; Căn cứ Quyết định số 20/QĐ-HĐĐH ngày 31 tháng 7 năm 2020 của Hội đồng Đại học Huế ban hành Quy chế tổ chức và hoạt động của Đại học Huế; Quyết định số 07/QĐ-HĐĐH ngày 19 tháng 01 năm 2021 của Hội đồng Đại học Huế sửa đổi, bổ sung một số điều của Quy chế tổ chức và hoạt động của Đại học Huế; Căn cứ Nghị quyết số 45/NQ-HĐĐH ngày 06 tháng 8 năm 2021 của Hội đồng Đại học Huế ban hành Quy chế hoạt động của Hội đồng Đại học Huế nhiệm kỳ 2021 - 2026; Căn cứ Quyết định số 06/QĐ-HĐĐH ngày 19 tháng 01 năm 2021 của Hội đồng Đại học Huế ban hành Quy định công nhận, bổ nhiệm, bổ nhiệm lại, kéo dài thời gian giữ chức vụ, thôi giữ chức vụ, miễn nhiệm, luân chuyển và chế độ phụ cấp chức vụ đối với viên chức quản lý tại Đại học Huế; Căn cứ Nghị quyết số 87/NQ-HĐĐH ngày 08 tháng 12 năm 2021.",
+    publisher: null,
+    to: listToData,
+    urgentLevel: i % 3 === 0 ? "Khẩn cấp" : "Bình thường",
   });
 }
 export default function SentDocument() {
+  const [filterType, setFilterType] = React.useState("all");
   const navigate = useNavigate();
+
   return (
-    <Row>
-      <ColAnt span={24}>
-        <Card size="small">
-          <List
-            size="small"
-            pagination={{
-              onChange: (page) => {
-                console.log(page);
-              },
-              pageSize: 10,
-            }}
-            dataSource={listForwardData}
-            renderItem={(item) => (
-              <>
-                <Col span={7} onClick={() => navigate(`detail/${item.id}`)}>
+    <Card size="small">
+      <List
+        size="small"
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 10,
+        }}
+        dataSource={listInboxData}
+        renderItem={(item) => (
+          <Badge.Ribbon
+            text={item.urgentLevel}
+            color={item.urgentLevel === "Bình thường" ? "green" : "red"}
+          >
+            <CardAnt>
+              <Row
+                align="middle"
+                justify="space-between"
+                onClick={() => navigate(`detail/${item.id}`)}
+              >
+                <Col span={24}>
                   <List.Item>
                     <List.Item.Meta
-                      avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-                      title={<Typography.Text strong>{item.title}</Typography.Text>}
-                      description={`Đến: ${item.to.email}`}
+                      avatar={
+                        <Avatar.Group
+                          maxCount={2}
+                          maxPopoverTrigger="click"
+                          size="large"
+                          maxStyle={{
+                            color: "#f56a00",
+                            backgroundColor: "#fde3cf",
+                            cursor: "pointer",
+                          }}
+                        >
+                          <Avatar>K</Avatar>
+                        </Avatar.Group>
+                      }
+                      title={
+                        <Link to={`${item.id}`}>
+                          <Typography.Text strong>{item.title}</Typography.Text>
+                        </Link>
+                      }
+                      description={
+                        <Typography.Text type="secondary">
+                          <Typography.Paragraph ellipsis={{ rows: 1 }}>
+                            <Typography.Text type="secondary">Đến: </Typography.Text>
+                            {_.map(item.to, "name").join(", ")}
+                          </Typography.Paragraph>
+                        </Typography.Text>
+                      }
                     />
                   </List.Item>
                 </Col>
-                <Col flex="auto" onClick={() => navigate(`detail/${item.id}`)}>
-                  {item.myMessage}
+                <Col span={24}>
+                  <Typography.Paragraph ellipsis={{ rows: 3 }}>{item.summary}</Typography.Paragraph>
                 </Col>
-                <Col flex="auto" onClick={() => navigate(`detail/${item.id}`)}>
-                  Forward từ {item.publisher.email}
-                </Col>
-                <Col flex="auto" onClick={() => navigate(`detail/${item.id}`)}>
-                  <Typography.Text>22/2/2022</Typography.Text>
-                </Col>
-              </>
-            )}
-          />
-        </Card>
-      </ColAnt>
-    </Row>
+              </Row>
+            </CardAnt>
+          </Badge.Ribbon>
+        )}
+      />
+    </Card>
   );
 }
