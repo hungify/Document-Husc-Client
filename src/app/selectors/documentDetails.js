0c72841bb5f9f9a40610e8eec6e1350a0f2b4af1
hungpurdie <nmhung2022@gmail.com>
2022-05-23 10:34:17 +0700

    feat(chat): new tab chat in document detail

--- src/app/selectors/documentDetails.js
+++ src/app/selectors/documentDetails.js
@@ -6,8 +6,6 @@ export const getProperty = (state) => state.documentDetails?.property;
 export const getFiles = (state) => state.documentDetails?.fileList;
 export const getRelatedDocuments = (state) => state.documentDetails?.relatedDocuments;
 export const getParticipants = (state) => state.documentDetails?.participants;
-export const getConversationId = (state) => state.documentDetails?.conversation?.conversationId;
-export const getMessages = (state) => state.documentDetails?.conversation?.messages;
 const getAnalytics = (state) => state.documentDetails?.analytics;
 export const getDatasets = createSelector(getAnalytics, (a) => {
   return [a.read?.count, a.unread?.count];
@@ -20,6 +18,22 @@ const getUnreaders = createSelector(getAnalytics, (a) => {
 });
 export const getReadAndUnread = createSelector(getReaders, getUnreaders, (readers, unreaders) => {
   if (readers && unreaders) {
-    return [...readers, ...unreaders];
+    const newReaders = readers.map((item) => {
+      return {
+        ...item,
+        key: item._id,
+      };
+    });
+
+    const newUnreaders = unreaders.map((item) => {
+      return {
+        ...item,
+        key: item._id,
+      };
+    });
+    return [...newReaders, ...newUnreaders];
   }
 });
+
+export const getConversationId = (state) => state.documentDetails?.conversationId;
+export const getMessages = (state) => state.documentDetails?.messages;
