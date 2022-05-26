41506942787298823a5381333c041a833431fea3
hungpurdie <nmhung2022@gmail.com>
2022-05-26 16:45:26 +0700

    fix: auto fetch documents list when login and logout success

--- src/features/Home/Home.jsx
+++ src/features/Home/Home.jsx
@@ -1,16 +1,18 @@
+import { isAuthenticated } from "app/selectors/auth";
 import ListDocument from "components/DocumentList";
 import HeaderListDocument from "components/HeaderListDocument";
 import { fetchDocuments } from "features/ManageDocuments/documentsSlice";
 import SearchGroup from "features/SearchGroup/SearchGroup";
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Home() {
   const dispatch = useDispatch();
+  const authenticated = useSelector(isAuthenticated);
 
   React.useEffect(() => {
     dispatch(fetchDocuments());
-  }, [dispatch]);
+  }, [dispatch, authenticated]);
 
   return (
     <>
